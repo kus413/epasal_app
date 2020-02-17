89f3bc86da5e08d22ab3ceedd7a5ef98526133ab
Kushal Shrestha <kushalshrestha85@gmail.com>
2020-02-17 15:05:22 +0545

    State management

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
@@ -9,7 +10,23 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageURL),
-    );
+        child: CachedNetworkImage(
+          imageUrl: imageURL,
+          fit: BoxFit.cover,
+          placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          title: Text(title, textAlign: TextAlign.center),
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+        ));
   }
 }
